58d0d564b5a059923a39ebe9169f438fa646d6a5
rottenkitkat <williyantohuang97@yahoo.com>
2021-01-03 14:50:06 +0700

    added submit form check and bootstrap breakpoints

--- script.js
+++ script.js
@@ -2,20 +2,37 @@ const nameInput = document.querySelector("#name");
 const email = document.querySelector("#email");
 const phone = document.querySelector("#phone");
 const zip = document.querySelector("#zip");
+const submitBtn = document.querySelector("#submit");
+
+let isNameValid = false;
+let isEmailValid = false;
+let isPhoneValid = false;
+let isZipValid = false;
 
 function validate(inputForm, fName) {
   if (fName()) {
     inputForm.classList.remove("is-invalid");
+    return true;
   } else {
     inputForm.classList.add("is-invalid");
+    return false;
   }
 }
 
-nameInput.addEventListener("blur", () => validate(nameInput, validateName));
-email.addEventListener("blur", () => validate(email, validateEmail));
-phone.addEventListener("blur", () => validate(phone, validatePhone));
-zip.addEventListener("blur", () => validate(zip, validateZip));
-
+nameInput.addEventListener(
+  "keyup",
+  () => (isNameValid = validate(nameInput, validateName))
+);
+email.addEventListener(
+  "keyup",
+  () => (isEmailValid = validate(email, validateEmail))
+);
+phone.addEventListener(
+  "keyup",
+  () => (isPhoneValid = validate(phone, validatePhone))
+);
+zip.addEventListener("keyup", () => (isZipValid = validate(zip, validateZip)));
+submitBtn.addEventListener("click", (e) => submit(e));
 function validateName() {
   const regex = /^[a-zA-Z ]{2,}$/;
   return regex.test(nameInput.value);
@@ -31,4 +48,34 @@ function validatePhone() {
 function validateZip() {
   const regex = /^\d{5}$/;
   return regex.test(zip.value);
+}
+
+function submit(e) {
+  e.preventDefault();
+  let isFormValid = [isNameValid, isEmailValid, isPhoneValid, isZipValid].every(
+    (arr) => arr === true
+  );
+  clearAlerts();
+  const alertDiv = document.createElement("div");
+  if (isFormValid) {
+    alertDiv.className = "alert alert-success mt-3";
+    alertDiv.textContent = "Form Submitted";
+  } else {
+    alertDiv.className = "alert alert-danger mt-3";
+    alertDiv.textContent = "Submit failed, please check your form ";
+  }
+  submitBtn.insertAdjacentElement("afterend", alertDiv);
+
+  setTimeout(() => {
+    alertDiv.classList.add("done");
+  }, 2000);
+  setTimeout(() => {
+    alertDiv.remove();
+  }, 3000);
+}
+function clearAlerts() {
+  const alert = document.querySelector(".alert");
+  if (alert) {
+    alert.remove();
+  }
 }
